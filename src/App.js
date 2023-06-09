8c76a39c5d11c72b735eac2ec5f6fe7f79bf06c8
RINKALRAJAN <rinkalranjan2468@gmail.com>
2023-06-09 16:52:18 +0530

    techystar

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Home from"./components/Home";
+import Services from "./components/Services";
+import "./styles/App.scss";
+import "./styles/contact.scss";
+import "./styles/footer.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
+import "./styles/mediaquery.scss";
+function App(){
+  return(
+<Router>
+  <Header />
+  <Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="contact" element={<Contact/>}/>
+  <Route path="/services" element={<Services />} />
+  </Routes>
+  <Footer />
+</Router>
+  );
+}
+export default App;
